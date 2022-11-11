da01e558d5b75a8cd0e6b3bb00399776cf187598
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-11-11 22:43:55 +0530

    Create function2.js

---
+++ Practice/function2.js
@@ -0,0 +1,8 @@
+//Passing argument from function calling according to ES-6
+//ES-6 bole toh back tick `` and using $ sign
+
+
+const fun1 = (name) => {
+    console.log(`I am ${name}`);
+}
+fun1('Aman');
